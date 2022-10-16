5ea21f3217a5fcac55e228c0a438719404997f45
shcharbakhin <113371130+shcharbakhin@users.noreply.github.com>
2022-10-16 23:05:43 +0300

    Решение задач 56,58,60

---
+++ Задача 56.cs
@@ -0,0 +1,49 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+{
+    Random rand = new Random();
+    int m = rand.Next(2, 10);
+    Console.WriteLine($"количество строк: {m}");
+    int n = rand.Next(2, 10);
+    Console.WriteLine($"количество столбцов: {n}");
+    int[,] array = new int[m, n];
+    int i;
+    int j;
+    int sum;
+    int min = 100;
+    int minstring=0;
+    //значение максиамально возможной суммы элементов массива исходя из размера рандома
+    string[] find = new string[n];
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            array[i, j] = rand.Next(1, 10);
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("Сумма всех элементов каждой строки:");
+    for (i = 0; i < m; i++)
+    {
+        sum = 0;
+        for (j = 0; j < n; j++)
+        {
+            sum = sum + array[i, j];
+        }
+        Console.WriteLine(sum);
+        find[i] = Convert.ToString(sum);
+        if (sum < min)
+        {
+            min = sum;
+            minstring = i+1;
+        }
+    }
+    Console.WriteLine($"Наименьшая сумма элементов в строке {minstring}");
+}
---
+++ Задача 60.cs
@@ -0,0 +1,29 @@
+﻿{
+    Random rand = new Random();
+    string[,,] array = new string[2, 2, 2];
+    string[] uniq = new string[8];
+    // массив для проверки (величина = умножение длины, ширины и высоты массива
+    for (int x = 0; x < 2; x++)
+    {
+        for (int y = 0; y < 2; y++)
+        {
+            for (int z = 0; z < 2; z++)
+            {
+                string num = Convert.ToString(rand.Next(10, 99));
+                int index = System.Array.IndexOf(uniq, num);//ищу строку в массиве
+                if (index < 0)
+                    if (index < 0)
+                    {
+                        array[x, y, z] = num;
+                        Console.Write(array[x, y, z]);
+                        Console.Write($"({x},{y},{z}); ");
+                    }
+                    else
+                    {
+                        z--;
+                    }
+                Console.WriteLine();
+            }
+        }
+    }
+}
